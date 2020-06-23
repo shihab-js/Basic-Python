7d43bb134c3c2204a4b817c8f49b33ee38b08b83
shihabrayhan8 <shihabrayhan8@gmail.com>
2020-06-24 03:07:00 +0600

    This file work done!

--- primeNumber.py
+++ primeNumber.py
@@ -1,3 +1,10 @@
-print("Hello")
 
+def isPrime(num):
+    if num % 2 == 0:
+        print("Congratulations! You enter a prime number.")
+    else:
+        print("Congratulations! You enter a non-prime number.")
 
+num = int(input("Please inter a integer number: "))
+
+isPrime(num)
