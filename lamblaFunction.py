79fab7ee82ac5aea8a0db56e4762e23bfd094c34
shihabrayhan8 <shihabrayhan8@gmail.com>
2020-06-24 03:55:22 +0600

    Lambda function done!

--- lamblaFunction.py
+++ lamblaFunction.py
@@ -1,1 +1,7 @@
+num = int(input("Please enter a integer number: "))
+result = lambda num : num % 2
 
+if result(num) == 0:
+    print("Congratulations! You Enter a prime number.")
+else:
+    print("Congratulations! You Enter a non-prime number.")
